98e155a7da52fc4158e398e697c7f14634a549af
gaby.com <gabylimon63@gmail.com>
2021-08-05 17:34:11 -0500

    Reajuste medidas, cambiarlas

--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -3,7 +3,7 @@ var tamWidh = [150,150,150,150,150,150,150,150,150];
 var tamHeight = [250,250,250,250,250,250,250,250,250];
 /*Recorrido */
 for(var i=0;i<piezas.length;i++){
-	piezas[i].setAttribute("width", tamWidh[i]);
+	piezas[i].setAttribute("width", tamWidh[i]); /*Tamaño cada pieza */
 	piezas[i].setAttribute("height",tamHeight[i]);
     /*Random */
     piezas[i].setAttribute("x", Math.floor((Math.random() * 10) + 1));
@@ -19,8 +19,8 @@ var currentPosX = 0;
 var currentPosY = 0;
 /*Declaracion mmov */
 function seleccionarElemento(evt) {
-	elementSelect = reordenar(evt);
-	currentX = evt.clientX;        
+	elementSelect = reordenar(evt); /*Almacenar elemento de cada pieza */
+	currentX = evt.clientX; /*Guardar la pos del mouse instantaneo */        
 	currentY = evt.clientY;
 	currentPosx = parseFloat(elementSelect.getAttribute("x"));   /*Obtencion de valor */   
 	currentPosy = parseFloat(elementSelect.getAttribute("y"));
@@ -28,57 +28,48 @@ function seleccionarElemento(evt) {
 }
 /*Distancia de recorrido */
 function moverElemento(evt){
-	var dx = evt.clientX - currentX;
+	var dx = evt.clientX - currentX; /*calcular la distancia */   
 	var dy = evt.clientY - currentY;
-	currentPosx = currentPosx + dx;
+	currentPosx = currentPosx + dx; /*Pos almacenada */   
 	currentPosy = currentPosy + dy;
 	elementSelect.setAttribute("x",currentPosx);/*Actualizar pos */
 	elementSelect.setAttribute("y",currentPosy);
 	currentX = evt.clientX;        
 	currentY = evt.clientY;	
-    elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
+    elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)"); /*Poder quitar seleccionar */
 	elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
-	iman();
 }
-function deseleccionarElemento(evt){
+function deseleccionarElemento(evt){ /*mover y soltar */   
     checar();
     if(elementSelect != 0){			
-		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmousemove"); 
 		elementSelect.removeAttribute("onmouseout");
 		elementSelect.removeAttribute("onmouseup");
 		elementSelect = 0;
 	}
 }
 var enviroment = document.getElementById('enviroment');
-function reordenar(evt){
-    var father =evt.target.parentNode;
+function reordenar(evt){ 
+    var father =evt.target.parentNode; 
     var clone=father.cloneNode(true);
     var id= father.getAttribute("id");
-    enviroment.removeChild(document.getElementById(id));
+    enviroment.removeChild(document.getElementById(id)); /*eliminar pieza de select */   
 	enviroment.appendChild(clone);
-	return enviroment.lastChild.firstChild;
+	return enviroment.lastChild.firstChild; /*poder sobreponer */   
 }
 
-var origX = [200,200,200,200,200,200,200,200,200];  /*Reajustar medidas */ 
+var origX = [200,200,200,200,200,200,200,200,200];  /*Reajustar medidas URGENTE, CHECAR*/ 
 var origY = [250,250,250,250,250,250,250,250,250];
 
-function iman(){
-	for(var i=0;i<piezas.length;i++){ /*Recorrido pieza */
-		if (Math.abs(currentPosx-origX[i])<15 && Math.abs(currentPosy-origY[i])<15) {
-			elementSelect.setAttribute("x",origX[i]);
-			elementSelect.setAttribute("y",origY[i]);
-		}
-	}
-}
 var audi = document.getElementById("audi");
 
-function checar() {
+function checar() {    
 	var good = 0;
 	var fathers = document.getElementsByClassName('father');
 	for(var i=0;i<piezas.length;i++){
 		var posx = parseFloat(fathers[i].firstChild.getAttribute("x"));    
 		var posy = parseFloat(fathers[i].firstChild.getAttribute("y"));
-		ide = fathers[i].getAttribute("id");
+		ide = fathers[i].getAttribute("id"); /*indice */   
 		if(origX[ide] == posx && origY[ide] == posy){
 			good = good + 1;
 		}
